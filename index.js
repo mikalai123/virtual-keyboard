a79094db8f838c3f05988b214a5c0a26d7c8fcbc
Mikalai Kavalionak <mikalai.kavalionak@gmail.com>
2022-05-10 00:41:52 +0300

    feat:add title

---
+++ index.js
@@ -0,0 +1,5 @@
+let div = document.createElement('div');
+  div.className = "title";
+  div.id = "title"
+  div.innerHTML = "<strong>RSS Виртуальная клавиатура</strong>";
+  document.body.append(div);
